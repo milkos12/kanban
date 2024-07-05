aece866ed2910177e5b179d1c6dd526ca9f9cdc0
Miller Ostos <millerandresoc@gmail.com>
2024-07-05 16:08:25 -0500

    add based for Droppable

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,20 +1,38 @@
 import React from 'react';
 import { useKanban } from '../context/KanbanContext';
 import { Column } from './Column'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 
 export const Board = () => {
-    
-    const {columns} = useKanban();
-    
-    
+
+    const { columns } = useKanban();
+
+    let conunt = 0;
     return (
-        <div>
-            {
-                columns.map(column =>
-                    <Column column={column}/>
-                )
-            }
-        </div>
+        <DragDropContext>
+            <Droppable>
+                {(provide) => (
+                    <div  ref={provide.innerRef} {...provide.droppableProps}>
+                        {
+                            columns.map(column => {
+                                conunt+=1;
+                                return (
+
+                                    <Draggable>
+                                        <Column column={column} key={conunt}/>
+                                    </Draggable>
+
+                                )
+                            }
+
+                            )
+                        }
+                        {provide.placeholder}
+                    </div>
+                )}
+            </Droppable>
+        </DragDropContext>
+
     )
 }
 
